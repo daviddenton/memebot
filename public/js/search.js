2124a6e2cad63a92726956909b81ca5069a83831
david denton <david.denton@springer.com>
2014-12-29 14:49:10 +0000

    dave - added copying of JSON template data to clipboard on search page

--- public/js/search.js
+++ public/js/search.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('button.copy').zclip({
         path: '/lib/zeroclip/ZeroClipboard.swf',
         copy: function (event) {
-            return document.location.origin + $(event.target).attr('content');
+            return $(event.target).attr('content');
         },
         afterCopy: function () {
         }
