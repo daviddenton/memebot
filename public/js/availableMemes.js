37f5e42e9a2cd3f28aa7f38d21c340a95f67d117
david denton <david.denton@springer.com>
2014-12-29 15:55:19 +0000

    dave - added toastr popup to confirm when clipboard copy is done

--- public/js/availableMemes.js
+++ public/js/availableMemes.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     $('button.copy').zclip({
-        path: '/lib/zeroclip/ZeroClipboard.swf',
+        path: 'http://cdnjs.cloudflare.com/ajax/libs/zclip/1.1.2/ZeroClipboard.swf',
         copy: function (event) {
             return document.location.origin + $(event.target).attr('content');
         },
-        afterCopy: function () {
+        afterCopy: function (event) {
+            toastr.success('\'' + $(event.target).attr('name') + '\' URL template copied to clipboard');
         }
     });
 });
--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     $('button.copy').zclip({
-        path: '/lib/zeroclip/ZeroClipboard.swf',
+        path: 'http://cdnjs.cloudflare.com/ajax/libs/zclip/1.1.2/ZeroClipboard.swf',
         copy: function (event) {
             return $(event.target).attr('content');
         },
-        afterCopy: function () {
+        afterCopy: function (event) {
+            toastr.success('\'' + $(event.target).attr('name') + '\' JSON template copied to clipboard');
         }
     });
 });
