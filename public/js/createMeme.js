bde6957f205d4ed4946fe5bc35ac3884baaa767b
david denton <david.denton@springer.com>
2015-01-02 11:25:37 +0000

    dave - simplified passing mapping to form

--- public/js/createMeme.js
+++ public/js/createMeme.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $('img.meme').click(function (e) {
         var target = $(e.target);
+        console.log(target.attr('mapping'));
+        var mapping = JSON.parse(target.attr('mapping'));
         $('.create-meme .title').text('Create meme from: ' + target.attr('name'));
-        $('.create-meme #generatorId').val(target.attr('generatorId'));
-        $('.create-meme #imageId').val(target.attr('imageId'));
+        $('.create-meme #generatorId').val(mapping.generatorId);
+        $('.create-meme #imageId').val(mapping.imageId);
         $('.create-meme img').attr('src', target.attr('src'));
         Custombox.open({
             target: '.create-modal',
