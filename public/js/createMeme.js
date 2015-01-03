38bd9bcd21db93cd43fd70028ca2ac93a71c80bd
david denton <david.denton@springer.com>
2015-01-03 15:09:25 +0000

    dave - dynamic rendering of caption templates

--- public/js/createMeme.js
+++ public/js/createMeme.js
@@ -1,17 +1,50 @@
+var TOKEN_PATTERN = /\%(\d+)/g;
+
+var MODAL_PROPERTIES = {
+    target: '.create-modal',
+    effect: 'flip',
+    overlaySpeed: 100,
+    speed: 200
+};
+
+function substituteCaptionsInto(template, textAreas) {
+    return _.reduce(textAreas, function (memo, textArea) {
+        var $textArea = $(textArea);
+        return memo.replace($textArea.attr('name'), $textArea.val());
+    }, template);
+}
+
+function createCaptionTextAreas(mappingInFocus) {
+    return _.chain((mappingInFocus.topText + mappingInFocus.bottomText).match(TOKEN_PATTERN) || []).unique().sortBy().map(function (token) {
+        return $('<textarea>', {name: token, placeholder: token}).addClass('control');
+    }).value();
+}
+
 $(document).ready(function () {
+
+    var mappingInFocus = {};
+
     $('img.meme').click(function (e) {
         var target = $(e.target);
-        var mapping = JSON.parse(target.attr('mapping'));
-        $('.create-meme .title').text('Create meme from: ' + target.attr('name'));
-        $('.create-meme #generatorId').val(mapping.generatorId);
-        $('.create-meme #imageId').val(mapping.imageId);
+        mappingInFocus = JSON.parse(target.attr('mapping'));
+
         $('.create-meme img').attr('src', target.attr('src'));
-        Custombox.open({
-            target: '.create-modal',
-            effect: 'flip',
-            overlaySpeed: 100,
-            speed: 200
-        });
+        $('.create-meme .title').text('Create meme from: ' + target.attr('name'));
+        $('.create-meme .templates').empty().append(createCaptionTextAreas(mappingInFocus));
+
+        Custombox.open(MODAL_PROPERTIES);
         e.preventDefault();
+    });
+
+    $('.create-meme button').click(function () {
+        var mappingToPost = _.extend(mappingInFocus);
+
+        var templates = $('textarea.control');
+        mappingToPost.topText = substituteCaptionsInto(mappingInFocus.topText, templates);
+        mappingToPost.bottomText = substituteCaptionsInto(mappingInFocus.bottomText, templates);
+
+        $.post('/', mappingToPost, function (url) {
+            window.location = url;
+        });
     });
 });
